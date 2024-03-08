c67447c2c102dfbb498af40bef69b8e5d84e3f3e
kenryu <jryu1521@gmail.com>
2024-03-08 15:29:53 +0900

    commit

--- src/main/java/sleepjo/run/Application.java
+++ src/main/java/sleepjo/run/Application.java
@@ -15,7 +15,7 @@ public class Application {
         Scanner sc = new Scanner(System.in);
         int input;
         do {
-            System.out.println("1. 전체 메뉴 조회\n 2. 메뉴 등록\n3. 메뉴 삭제\n4. 메뉴 업데이트\n5. 프로그램 종료");
+            System.out.println("1. 전체 메뉴 조회\n2. 메뉴 등록\n3. 메뉴 삭제\n4. 메뉴 업데이트\n5. 프로그램 종료");
             System.out.println("========================");
             System.out.print("입력: ");
             input = sc.nextInt();
@@ -23,7 +23,24 @@ public class Application {
                 System.out.println("잘못된 입력값 입니다.");
                 System.out.println("============================");
             }
+
+            switch (input){
+                case 1 : // 전체메뉴조회
+                    break;
+                case 2: // 메뉴 등록
+                    break;
+                case 3: // 메뉴 삭제
+                    break;
+                case 4: // 메뉴 업데이트
+            }
+
+
+
+
+
+
         } while(input != 5);
+        System.out.println("프로그램이 종료 됩니다.");
 
     }
 }
